0fa798f5e702f6e70e1c5b88edb2eb9184b09b80
Moritz Klaiber <67148813+mklaiber@users.noreply.github.com>
2023-03-26 20:58:53 +0200

    Updated configuration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "spacekioskng92357031": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
