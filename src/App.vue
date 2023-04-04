321af3bb51dc4a67c73f0eff385f251d85d252c3
Marci2070 <marcel.schramberg@gmail.com>
2023-04-04 22:21:05 +0200

    added simple darkmode because why not

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+  setup() {
+    const $q = useQuasar();
+
+    //set dark mode if browser prefers dark color scheme
+    $q.dark.set(window.matchMedia("(prefers-color-scheme: dark)").matches);
+  },
+});
 </script>
